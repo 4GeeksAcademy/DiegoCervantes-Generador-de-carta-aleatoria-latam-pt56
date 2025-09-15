12dac73fcd05b0fc6d4044d9a19357e6cf559a09
Diego cervantes <diegocerbta@gmail.com>
2025-09-15 04:39:50 +0000

    First

--- src/app.js
+++ src/app.js
@@ -4,7 +4,11 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
+function RandomCard () {
+  const palos = ['♣', '♥', '♠', '♦',]
+  const valores = ['1','2','3','4','5','6','7','8','9','J','Q','K','A'];
+  const palo = palos [Math.floor(Math.random() * palos.length)];
+} 
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
