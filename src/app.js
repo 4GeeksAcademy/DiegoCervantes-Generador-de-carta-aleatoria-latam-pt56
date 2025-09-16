453ee9b072ff892f0ca188ba7d93da7d4a1d1d06
Diego cervantes <diegocerbta@gmail.com>
2025-09-16 00:28:39 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -8,8 +8,17 @@ function RandomCard () {
   const palos = ['♣', '♥', '♠', '♦',]
   const valores = ['1','2','3','4','5','6','7','8','9','J','Q','K','A'];
   const palo = palos [Math.floor(Math.random() * palos.length)];
-} 
+  const valor = valores[Math.floor(Math.random() * valores.length)]; 
+  
+  document.getElementById(`top`).textContent = palo
+  document.getElementById(`bottom`).textContent = palo
+  document.getElementById(`value`).textContent = valor
+
+  document.getElementById(`card`).style.color = (palo === "♥" || palo === "♦") ? 'red' : 'black'
+}
+
 window.onload = function() {
   //write your code here
+  RandomCard ();
   console.log("Hello Rigo from the console!");
 };
